ba0d3fddc9d868cc82f760c17180b7a372ebd0b6
bdattoma <bdattoma@redhat.com>
2025-08-29 11:56:37 +0200

    add quiet + no grace

--- delete_resources.sh
+++ delete_resources.sh
@@ -8,45 +8,45 @@ function update_labels() {
     echo ">> Compute Engine: instances (VMs)"
     gcloud compute instances list --filter "$FILTER" --format="value(name,zone,creationTimestamp)" | while read -r instance zone creationTimestamp; do
         echo "[INFO] Deleting instance: $instance in zone: $zone created at: $creationTimestamp"
-        gcloud compute instances delete $instance --zone=$zone
+        gcloud beta compute instances delete $instance --zone=$zone --no-graceful-shutdown --quiet
     done
 
    echo ">> Compute Engine: images"
    gcloud compute images list --filter "$FILTER" --format="json" | jq --arg PROJECT $PROJECT '.[] | select(.selfLink | contains($PROJECT)) | .name' | tr -d '"' | while read -r image; do
         echo "[INFO] Deleting image: $image"
-        gcloud compute images delete $image
+        gcloud compute images delete $image --quiet
     done
 
     echo ">> Compute Engine: disks"
     gcloud compute disks list --filter "$FILTER" --format="value(name,zone,creationTimestamp)" | while read -r disk zone creationTimestamp; do
         echo "[INFO] Deleting disk: $disk in zone: $zone created at: $creationTimestamp"
-        gcloud compute disks delete $disk --zone=$zone
+        gcloud compute disks delete $disk --zone=$zone --quiet
     done
 
     echo ">> Compute Engine: snapshots"
     gcloud compute snapshots list --filter "$FILTER" --format="value(name,creationTimestamp)" | while read -r snapshot creationTimestamp; do
         echo "[INFO] Deleting snapshot: $snapshot created at: $creationTimestamp"
-        gcloud compute snapshots delete $snapshot
+        gcloud compute snapshots delete $snapshot --quiet
     done
 
     echo ">> Networking: Forwarding Rules"
     gcloud compute forwarding-rules list --filter "$FILTER" --format="value(name,region,creationTimestamp)" | while read -r forwardingrule region creationTimestamp; do
         echo "[INFO] Deleting forwarding rule: $forwardingrule in region: $region created at: $creationTimestamp"
-        gcloud compute forwarding-rules delete $forwardingrule --region=$region
+        gcloud compute forwarding-rules delete $forwardingrule --region=$region --quiet
     done
     # TODO: Add support for global IP addresses, no region
 
     echo ">> Networking: External IP Addresses"
     gcloud compute addresses list --filter "$FILTER" --format="value(name,region,creationTimestamp)" | while read -r address region creationTimestamp; do
         echo "[INFO] Deleting external IP address: $address in region: $region created at: $creationTimestamp"
-        gcloud compute addresses delete $address --region=$region
+        gcloud compute addresses delete $address --region=$region --quiet
     done
     # TODO: Add support for global IP addresses, no region
 
     echo ">> Networking: VPN tunnels"
     gcloud compute vpn-tunnels list --filter "$FILTER" --format="value(name,region,creationTimestamp)" | while read -r tunnel region creationTimestamp; do
         echo "[INFO] Deleting VPN tunnel: $tunnel in region: $region created at: $creationTimestamp"
-        gcloud compute vpn-tunnels delete $tunnel --region=$region
+        gcloud compute vpn-tunnels delete $tunnel --region=$region --quiet
     done
     
     if [[ "$rc" -ne 0 ]]; then
